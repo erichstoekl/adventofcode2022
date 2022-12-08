6e722f62351fc9faa3d5e0121595f08d971d3814
Erich Stoekl <erich.stoekl@treasure-data.com>
2022-12-08 17:54:13 +0900

    finally solved day 7

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -6,33 +6,44 @@ mod file_reader;
 #[macro_use] extern crate scan_fmt;
 extern crate rctree;
 
-fn recur_build_vector(root: &Node<(String, bool, u64)>, depth: usize, v: &mut Vec<u64>) -> (u64, Vec<u64>) {
+static mut global: u64 = 0;
+
+fn recur_build_vector(root: &Node<(String, bool, u64)>, depth: usize) -> u64 {
     let mut total: u64  = 0;
     let data = root.borrow();
     let s = String::from_str(&std::iter::repeat(" ").take(depth).collect::<String>()).unwrap();
-
+    /*
+    if data.1 {
+        println!("{}- {} (dir)", s, data.0);
+    } else {
+        println!("{}- {} (file, size={})", s, data.0, data.2);
+    }
+*/
     if !root.has_children() {
-        return (data.2);
+        return data.2;
     }
     let children = root.children();
     for child in children {
-        let (t, v) = recur_build_vector(&child, depth + 2, v);
+        let t = recur_build_vector(&child, depth + 2);
         total += t;
     }
+
     
     if total <= 100000 {
-        v.push(total);
-        (total, v.to_vec())
-    } else {
-        (0, v.to_vec())
+        println!("Total is {}", total);
+        unsafe {
+            global += total;
+        }
     }
+    total
 }
 
 fn solve_problem_one(filename: &str) {
     let root = file_reader::get_file_tree(filename);
-    let mut v = Vec::new();
-    recur_build_vector(&root, 0, &mut v);
-
+    recur_build_vector(&root, 0);
+    unsafe {
+        println!("Final result: {}", global);
+    }
 }
 
 fn solve_problem_two(filename: &str) {
